594e6c32ebfcd34c7f583767fbbe935d331d9ebc
Asa Holland <asaleholland@gmail.com>
2023-06-10 14:07:30 -0700

    feat(Initital Discord Skeleton): Fix implementation

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import discord
 from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.typing = False
@@ -7,12 +13,15 @@ intents.presences = False
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+
 @bot.event
 async def on_ready():
     print(f'Bot is ready. Logged in as {bot.user.name}')
+
 
 @bot.command()
 async def hello(ctx):
     await ctx.send('Hello!')
 
-bot.run('YOUR_BOT_TOKEN')
+
+bot.run(DISCORD_TOKEN)
