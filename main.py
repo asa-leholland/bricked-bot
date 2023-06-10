0e48785df553761cb0af21132dec0c6efd5bce7e
Asa Holland <asaleholland@gmail.com>
2023-06-10 15:55:30 -0700

    clean

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Bot is ready. Logged in as {bot.user.name}')
+    print(f'\nBot is ready. Logged in as {bot.user.name}')
 
 
 @bot.command()
@@ -30,16 +30,11 @@ async def hello(ctx):
 @bot.command()
 async def setimage(ctx, set_number):
     try:
-        # Make a GET request to the Rebrickable API to get set information
         headers = {'Accept': 'application/json', 'Authorization': f'key {REBRICKABLE_API_KEY}'}
         response = requests.get(f'https://rebrickable.com/api/v3/lego/sets/{set_number}/', headers=headers)
         if response.status_code == 200:
             data = response.json()
-
-            # Extract the image URL from the response
             image_url = data['set_img_url']
-
-            # Send the image as a response
             await ctx.send(image_url)
         else:
             await ctx.send('Failed to fetch set information.')
